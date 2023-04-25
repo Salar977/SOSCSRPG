22af8b3e2adea880f1f1f75cdd02bfc9bc2372bb
Salar Amir <Salar831@gmail.com>
2023-04-25 17:48:58 +0200

    added locations

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -7,18 +7,34 @@ using System.Threading.Tasks;
 
 namespace Engine.ViewModels;
 
-internal class GameSession
+public class GameSession
 {
-
+	public World CurrentWorld { get; set; }
 	public Player CurrentPlayer { get; set; }
+	public Location CurrentLocation { get; set; }
 
 	public GameSession()
 	{
 		CurrentPlayer = new Player
 		{
 			Name = "Salar",
-			Gold = 1000000
+			CharacterClass = Characters.Warrior.ToString(),
+			HitPoints = 10,
+			ExperiencePoints = 0,
+			Level = 1,
+			Gold = 25
 		};
+
+		CurrentLocation = new Location
+		{
+			XCoordinate = 0,
+			YCoordinate = -1,
+			Name = "Home",
+			Description = "This is your home!",
+			ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png"
+		};
+
+		CurrentWorld = new World();
 	}
 
 
